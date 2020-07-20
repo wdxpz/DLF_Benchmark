b2a00d34cc3b6ed4a83ef1662b8ca43289e211c0
wdxpz <wdxpz@qq.com>
2020-07-20 13:37:44 +0000

    [Paddle & Pytorch] rerunned some test

--- PyTorch/test_vgg16.py
+++ PyTorch/test_vgg16.py
@@ -4,3 +4,5 @@ from imageclassification.vgg16classifier import init, train, test
 device, vgg16, CIFAR_train_loader, CIFAR_test_loader = init(ImageClassifyConfig)
 train(device, vgg16, CIFAR_train_loader, ImageClassifyConfig)
 test(device, vgg16, CIFAR_test_loader, ImageClassifyConfig)
+
+
